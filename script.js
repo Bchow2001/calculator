a0c0f42ce8644e65c1a6a858b6176f15dde4d107
Bryan Chow <bchow.post@gmail.com>
2023-03-30 21:55:44 +0100

    Add button events and began calculate events
    To do:
        Fix and finish calculate
        add CSS styles

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var num1, num2, operator;
 
+var displayDigits = [];
+
+const display = document.querySelector("#display-output");
+
+const btns = document.querySelectorAll(".normal");
+
+const equal = document.querySelector("#equal");
+
+const cleanDigits = [];
+
 function add(a, b) {
     return a + b;
 }
@@ -17,13 +27,41 @@ function divide(a, b){
 }
 
 function operate(operator) {
-    if (operator === "add") {
+    if (operator === "+") {
         return add(num1, num2);
-    } else if (operator === "subtract") {
+    } else if (operator === "-") {
         return subtract(num1, num2);
-    } else if (operator === "multiply") {
+    } else if (operator === "*") {
         return multiply(num1, num2);
-    } else if (operator === "divide") {
+    } else if (operator === "/") {
         return divide (num1, num2);
     }
 }
+
+
+btns.forEach(btn => {
+    btn.addEventListener("click", event =>{
+        display.textContent += btn.textContent;
+        displayDigits.push(btn.id);
+    })
+});
+
+equal.addEventListener("click", calculate)
+
+function calculate() {
+    displayDigits.reduce((digits, digit) => {
+        if (digit != NaN){
+            return digits += digit.toString()
+        } else {
+            cleanDigits.push(digits);
+            cleanDigits.push(digit);
+            digits = null;
+        }
+        console.log(cleanDigits);
+        // edit this so that it appends digits as a whole
+        // and operators by themselves to cleanDigits
+    });
+}
+
+
+
